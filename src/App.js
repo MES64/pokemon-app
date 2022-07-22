5d718da9652e1f70afefe636e06396dc65cd2a1b
MES64 <markskelton10@gmail.com>
2022-07-22 22:30:16 +0100

    Created PokemonRow Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import PropTypes from 'prop-types';
 import './App.css';
 import pokemon from './pokemon.json';
+
+const PokemonRow = ({ pokemon }) => (
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(", ")}</td>
+  </tr>
+)
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  }),
+};
 
 function App() {
   return (
@@ -20,10 +37,7 @@ function App() {
         </thead>
         <tbody>
           {pokemon.slice(0, 20).map((pokemon) => (
-            <tr key={pokemon.id}>
-              <td>{pokemon.name.english}</td>
-              <td>{pokemon.type.join(", ")}</td>
-            </tr>
+            <PokemonRow pokemon={pokemon} key={pokemon.id}/>
           ))}
         </tbody>
       </table>
